4e51a32dc373837d6934b04fa9fd7d0d8e34a95a
Sean Mafnas <seanvincent671@gmail.com>
2025-09-21 21:32:00 +1000

    refactor: remove hardcoded color divs and adjust CSS for color display

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const colorPicker = document.getElementById("color-picker");
 
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("header__color-select-btn")) {
-    console.log(colorPicker.value);
+    // console.log(colorPicker.value);
+    const colorCodeValue = colorPicker.value;
+    const splitColorCode = colorCodeValue.split("#");
+    // console.log(splitColorCode);
+    fetch(`https://www.thecolorapi.com/scheme?hex=${splitColorCode}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const main = document.querySelector(".main");
+        let colorHTML = ``;
+        data.colors.forEach((color) => {
+          colorHTML += `<div class="color">${color.hex.value}</div>`;
+        });
+        main.insertAdjacentHTML("afterbegin", colorHTML);
+      });
   }
 });
